688c9c3d65d652314987dd1d9e49ab8068098107
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-03-10 13:57:43 +0100

    footer fixed

--- element/head.php
+++ element/head.php
@@ -4,6 +4,8 @@
 
     <link rel="stylesheet/less" type="text/css" href="less/main.less" />
     <script src="https://cdn.jsdelivr.net/npm/less" data-env="development"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
 
     <script
         src="https://code.jquery.com/jquery-3.6.0.min.js"

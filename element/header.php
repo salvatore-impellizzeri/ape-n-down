c73e109f1222db498058ef243120fe9a79508b69
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-03-11 16:32:56 +0100

    fixing header and view

--- element/header.php
+++ element/header.php
@@ -5,7 +5,10 @@
 <header class="header z-100">
     <button>
         <a href="/menu.php">
-            <?= file_get_contents('img/icons/menu.svg') ?> Menu
+            <?= file_get_contents('img/icons/menu.svg') ?>
+            <span>
+                Menu
+            </span>
         </a>
     </button>
     <div class="header__logo">
@@ -15,7 +18,10 @@
     </div>
     <button class="header__donation">
         <a href="/donation.php">
-            <?= file_get_contents('img/icons/heart.svg') ?> Dona ora
+            <?= file_get_contents('img/icons/heart.svg') ?> 
+            <span>
+                Dona ora
+            </span>
         </a>
     </button>
 

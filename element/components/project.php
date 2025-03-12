53ee65eb377a1bbf5ab43a9cfc9c83b9f33fd6ea
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-03-12 17:27:48 +0100

    Saving

--- element/components/project.php
+++ element/components/project.php
@@ -2,12 +2,10 @@
     function Project($date, $title, $img, $link) {
 ?>
     <div class="projects__single-project">
-        <div class="projects__single-projects__img">
-            <a href="<?= $link ?>">
-                <img src="<?= $img ?>" alt="<?= $title ?>">
-            </a>
+        <a href="<?= $link ?>" class="projects__single-projects__img">
+            <img src="<?= $img ?>" alt="<?= $title ?>">
             <?= file_get_contents('img/find.svg') ?>
-        </div>
+        </a>
         <div>
             <p>
                 <?= $date ?>

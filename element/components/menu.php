102da009de9ac17ed42ffa69abdd83218c9b8028
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-03-21 14:41:47 +0100

    menu responsive on most screens

--- element/components/menu.php
+++ element/components/menu.php
@@ -3,55 +3,57 @@
 ?>
 
 <div id="menu" class="menu-container">
-    <?php 
-        Menu_item(
-            "home",
-            "menu__item--r",
-            "/"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "eventi",
-            "menu__item--l",
-            "/progetti.php"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "dona ora",
-            "menu__item--r",
-            "/donation.php"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "parlano di noi",
-            "menu__item--l",
-            "/about.php"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "lavora con noi",
-            "menu__item--r",
-            "/work.php"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "partner",
-            "menu__item--l",
-            "/partner.php"
-        )
-    ?>
-    <?php
-        Menu_item(
-            "contatti",
-            "menu__item--r",
-            "/contacts.php"
-        )
-    ?>
+    <div class="menu-container__box">
+        <?php 
+            Menu_item(
+                "home",
+                "menu__item--r",
+                "/"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "eventi",
+                "menu__item--l",
+                "/progetti.php"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "dona ora",
+                "menu__item--r",
+                "/donation.php"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "parlano di noi",
+                "menu__item--l",
+                "/about.php"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "lavora con noi",
+                "menu__item--r",
+                "/work.php"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "partner",
+                "menu__item--l",
+                "/partner.php"
+            )
+        ?>
+        <?php
+            Menu_item(
+                "contatti",
+                "menu__item--r",
+                "/contacts.php"
+            )
+        ?>
+    </div>
 </div>
 
 <?php 
--- js/custom.js
+++ js/custom.js
@@ -138,99 +138,99 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // IMAGE FIRST TITLE
 
-    // document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
 
-    //     const image = document.querySelector(".container-title-img img");
-    //     const container2 = document.querySelector(".overlay-img");
+        const image = document.querySelector(".container-title-img img");
+        const container2 = document.querySelector(".overlay-img");
 
-    //     let hasAnimated = false;
-    //     let canScroll = false; // Flag per il controllo dello scroll
-    //     let firstScrollDone = false; // Flag per il primo scroll
+        let hasAnimated = false;
+        let canScroll = false; // Flag per il controllo dello scroll
+        let firstScrollDone = false; // Flag per il primo scroll
 
-    //     // Disabilita lo scroll all'inizio nella home
-    //     if (window.location.pathname === "/" || window.location.pathname === "/main.php") {
-    //         document.body.style.overflow = "hidden";
-    //     }    
+        // Disabilita lo scroll all'inizio nella home
+        if (window.location.pathname === "/" || window.location.pathname === "/main.php") {
+            document.body.style.overflow = "hidden";
+        }    
 
-    //     function startAnimation() {
-    //         if (!hasAnimated) {
+        function startAnimation() {
+            if (!hasAnimated) {
 
-    //             const state = Flip.getState(image);
+                const state = Flip.getState(image);
 
-    //             gsap.to(".square-s__img", {
-    //                 opacity: 1, 
-    //                 duration: 0.5,
-    //                 ease: "power2.inOut",
-    //                 onStart: function () {
-    //                     document.querySelector(".square-s__img").style.visibility = "visible";
-    //                 }
-    //             });
+                gsap.to(".square-s__img", {
+                    opacity: 1, 
+                    duration: 0.5,
+                    ease: "power2.inOut",
+                    onStart: function () {
+                        document.querySelector(".square-s__img").style.visibility = "visible";
+                    }
+                });
                 
 
-    //             if (window.innerWidth > 1920) {
-    //                 image.style.maxWidth = "1920px";
-    //                 image.style.display = "block";
-    //                 image.style.margin = "auto";
-    //             }
+                if (window.innerWidth > 1920) {
+                    image.style.maxWidth = "1920px";
+                    image.style.display = "block";
+                    image.style.margin = "auto";
+                }
 
-    //             container2.appendChild(image);
+                container2.appendChild(image);
 
-    //             Flip.from(state, {
-    //                 duration: 0.8,
-    //                 ease: "power2.inOut",
-    //                 scale: true,
-    //                 absolute: true,
-    //             });
+                Flip.from(state, {
+                    duration: 0.8,
+                    ease: "power2.inOut",
+                    scale: true,
+                    absolute: true,
+                });
 
-    //             gsap.to(".overlay-img__bg", {
-    //                 opacity: 1,
-    //                 duration: 0.2,
-    //                 ease: "power2.inOut",
-    //                 onComplete: function () {
-    //                     // Riabilita lo scroll dopo l'animazione
-    //                     document.body.style.overflow = "auto";
-    //                     canScroll = true; 
-    //                 }
-    //             });
+                gsap.to(".overlay-img__bg", {
+                    opacity: 1,
+                    duration: 0.2,
+                    ease: "power2.inOut",
+                    onComplete: function () {
+                        // Riabilita lo scroll dopo l'animazione
+                        document.body.style.overflow = "auto";
+                        canScroll = true; 
+                    }
+                });
 
-    //             hasAnimated = true;
-    //         }
-    //     }
+                hasAnimated = true;
+            }
+        }
 
-    //     // Attende 3,5 secondi prima di eseguire l'animazione
-    //     setTimeout(startAnimation, 3500);
+        // Attende 3,5 secondi prima di eseguire l'animazione
+        setTimeout(startAnimation, 3500);
 
-    //     // Aggiungi l'evento di scroll
-    //     window.addEventListener("wheel", function (event) {
-    //         if (canScroll && !firstScrollDone) {
+        // Aggiungi l'evento di scroll
+        window.addEventListener("wheel", function (event) {
+            if (canScroll && !firstScrollDone) {
 
-    //             // Imposta il flag per il primo scroll
-    //             firstScrollDone = true;
+                // Imposta il flag per il primo scroll
+                firstScrollDone = true;
 
-    //             // Imposta l'opacità di .overlay-img a 0 appena inizia lo scroll
-    //             gsap.to(".overlay-img", {
-    //                 opacity: 0,
-    //                 zIndex: -10,
-    //                 duration: 0.3, 
-    //                 delay: 0.3,
-    //                 ease: "power2.inOut",
-    //             });
+                // Imposta l'opacità di .overlay-img a 0 appena inizia lo scroll
+                gsap.to(".overlay-img", {
+                    opacity: 0,
+                    zIndex: -10,
+                    duration: 0.3, 
+                    delay: 0.3,
+                    ease: "power2.inOut",
+                });
 
-    //             let scrolled = false; 
+                let scrolled = false; 
 
-    //             window.addEventListener('scroll', () => {
-    //                 if (!scrolled && window.scrollY > 0) {
-    //                     window.scrollBy({
-    //                         top: window.innerHeight,
-    //                         behavior: 'smooth' 
-    //                     });
+                window.addEventListener('scroll', () => {
+                    if (!scrolled && window.scrollY > 0) {
+                        window.scrollBy({
+                            top: window.innerHeight,
+                            behavior: 'smooth' 
+                        });
 
-    //                     scrolled = true;
-    //                 }
-    //             });          
-    //         }
-    //     });
-    // });
+                        scrolled = true;
+                    }
+                });          
+            }
+        });
+    });
 
 
 // REMOVING ANIMATION ON FIRSTCARD 
